59dcedbb05b12f3c0f065e68ff20366e0b49eb95
Yoyofeng <2216179464@qq.com>
2018-02-01 10:23:17 +0800

    接口

--- modules/content/models/CategoryContent.php
+++ modules/content/models/CategoryContent.php
@@ -20,4 +20,24 @@ class CategoryContent extends ActiveRecord
 
         ];
     }
+
+    /**
+     * 将分类的内容的副分类存储
+     * @param $contentId 内容ID
+     * @param $category 副分类
+     * @Obelisk
+     */
+    public function secondClass($contentId, $category)
+    {
+        foreach ($category as $v) {
+            $model = new CategoryContent();
+            $saveData = [
+                'contentId' => $contentId,
+                'catId' => $v
+            ];
+            $model->setAttributes($saveData);
+            $model->save();
+        }
+        return $model->primaryKey;
+    }
 }
