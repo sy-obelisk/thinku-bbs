80e8a77e51eccc6a904289b305810228c733e566
Aaron Dai <daicunya@163.com>
2018-02-06 10:51:49 +0800

    发帖

--- web/cn/js/new-article.js
+++ web/cn/js/new-article.js
@@ -15,47 +15,54 @@ var _new = {
   },
   bind : function () {
     var _this = this;
-    // 检查分类是否有填写
-    document.querySelector('.new-title input').onblur = function () {
-      console.log('sss');
-      _this.checkInput(this);
-    };
+
+    // document.querySelector('.new-title input').onblur = function () {
+    //   console.log('sss');
+    //   _this.checkInput(this);
+    // };
     // 发表
     $('.put-in').click(function () {
-      console.log('a');
       _this.publishEvent();
     })
   },
+  //
   checkInput : function (obj) {
     var _this = this;
-    if ($('#clsFirst').val() == '帖子分类' || $('#clsSecond').val() == '二级分类' || ($('#clsThird').css('visibility') == 'visible' && $('#clsThird').val() == '三级分类')) {
-      alert("请选择帖子分类！");
-    } else {
-      if (!$(obj).val()) {
-        alert('请出入标题');
-      } else {
-        // 点击发表
 
-      }
-    }
   },
-  // 发表
+  // 检查分类是否有填写,发表
   publishEvent : function () {
-   $.ajax({
-     type: 'post',
-     url : "/cn/api/new-article",
-     data: {
-       name: 'fsdfsfd', // 标题
-       abstract: 'afa', // 摘要
-       pid: '0', // 父id
-       catId: '2', // 主 id
-       article: '222222', // 帖子内容
-       category: '7,21' // 子分类id
-     },
-     dataType : 'json',
-     success : function (res) {
-       console.log(res);
-     }
-   })
+    if ($('#clsFirst').val() == '帖子分类' || $('#clsSecond').val() == '二级分类' || ($('#clsThird').css('visibility') == 'visible' && $('#clsThird').val() == '三级分类')) {
+      alert("请选择帖子分类!");
+      return false;
+    }
+    if (!$('.new-title input').val()) {
+      alert('请出入标题!');
+      return false;
+    }
+    if(!ue.getContent()) {
+      alert('请输入帖子内容!')
+      return false;
+    }
+    var category = $('#clsSecond').val()+','+$('#clsThird').val();
+    console.log(category);
+    $.ajax({
+      type: 'post',
+      url : "/cn/api/new-article",
+      data: {
+        name: $('.new-title input').val(), // 标题
+        catId: $('#clsFirst').val(), // 主 id
+        article: ue.getContent(), // 帖子内容
+        category: category // 子分类id
+      },
+      dataType : 'json',
+      success : function (res) {
+        console.log(res);
+        if (res.code == 0) {
+          alert(res.message);
+          location.href = '/details/'+res.id+'.html';
+        }
+      }
+    })
   }
 }
