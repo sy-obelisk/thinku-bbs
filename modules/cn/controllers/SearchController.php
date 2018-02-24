d93e83e81d4e7eb0889ae4d0c1d3a5cbffc61eec
Yoyofeng <2216179464@qq.com>
2018-02-24 15:37:52 +0800

    1

--- modules/cn/controllers/SearchController.php
+++ modules/cn/controllers/SearchController.php
@@ -27,7 +27,7 @@ class SearchController extends Controller
         $keyword  =strip_tags($keyword);
         $pagesize=15;
         $offset=$pagesize*($page-1);
-        $data = Yii::$app->db->createCommand("select id,name,title,summary from {{%content}} where name like '%$keyword%' limit $offset,$pagesize")->queryAll();
+        $data = Yii::$app->db->createCommand("select c.id,c.name,c.abstract,c.viewCount,c.createTime,u.userName,u.nickname,u.image,(SELECT CONCAT_WS(' ',ce.value,ed.value) From {{%content_extend}} ce left JOIN {{%extend_data}} ed ON ed.extendId=ce.id WHERE ce.contentId=c.id AND ce.code='99b3cc02b18ec45447bd9fd59f1cd655')  as listeningFile from {{%content}} c LEFT JOIN {{%user}} u ON u.id=c.userId where name like '%$keyword%' order by id desc limit $offset,$pagesize")->queryAll();
         return $this->render('search',$data);
     }
 }
