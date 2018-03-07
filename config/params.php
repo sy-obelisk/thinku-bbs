35f7fb417e63f8ee6e08cfc886bbe6800dcde1e8
Aaron Dai <daicunya@163.com>
2018-03-07 15:25:19 +0800

    mothing

--- config/params.php
+++ config/params.php
@@ -10,18 +10,6 @@ return [
     'defaultImg' => '',
     'emailTimeLimit' => 24 * 3600,
     'phone1' => '您正在通过手机免费注册申友论坛！',
-//    'phone2' => '您正在通过手机预约申友网留学顾问！',
-//    'phone3' => '您正在通过手机预约申友网考试培训顾问！',
-//    'phone4' => '您正在通过手机免费注册申友网公开课会员！',
-//    'phone5' => '您正在通过手机找回申友公开课账号密码！',
-//    'phone6' => '您正在通过邮箱免费注册申友网公开课会员！',
-//    'phone7' => '您正在通过邮箱找回申友公开课账号密码！',
-//    'phone8' => '您正在修改绑定手机！',
-//    'phone9' => '您正在修改绑定邮箱！',
-//    'schoolUrl' => 'http://schools.smartapply.cn/cn/api/school',
-//    'categoryUrl' => 'http://schools.smartapply.cn/cn/api/cats',
-//    'contentUrl' => 'http://schools.smartapply.cn/cn/api/content',
-//    'schoolConnect' => 'http://schools.smartapply.cn',
 
     'report' => [
         1 => '垃圾营销',
