810f37667f44bd0d165b75f88b6cf449d6dfc90c
Aaron Dai <daicunya@163.com>
2018-03-16 15:41:21 +0800

    底部、关键词标题

--- commands/front/views/foot.php
+++ commands/front/views/foot.php
@@ -35,4 +35,19 @@
             </ul>
         </div>
     </div>
+    <div class="foot-text">
+        <p>
+        <b>友情链接：</b>
+        <a href="http://www.thinkugmat.com" target="_blank">申友在线|</a>
+        <a href="http://www.gmatonline.cn/" target="_blank">雷哥网|</a>
+        <a href="http://bbs.viplgw.cn/" target="_blank">留学社区|</a>
+        <a href="http://weibo.com/2592925901/profile?topnav=1&wvr=6" target="_blank">新浪微博|</a>
+        <a href="http://page.renren.com/601277497?checked=true" target="_blank">人人网</a>
+        </p>
+        <span>Copyright © 2015 All Right Reserved 申友教育 版权所有
+        <a target="_blank" href="http://www.miitbeian.gov.cn/">京ICP备16000003号</a>
+        <span>京公网安备 11010802018491</span>
+<!--        <a target="_blank" href="/cn/index/disclaimer">免责声明</a>-->
+    </span>
+    </div>
 </footer>
